08850aacbe3976f444dfdbcd58b43c2440bb4f51
Abhishek Magdum <abhishek.magdum.07@gmail.com>
2023-06-29 16:07:53 +0530

    Added : Adding remaining properties to DTO and model

--- src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,19 +1,31 @@
 package com.bridgelabz.employeepayroll.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-
+import javax.validation.constraints.*;
+import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class EmployeePayrollDTO {
     @NotBlank(message = "Employee name cannot be null")
-    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}$")
+    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}$", message = "Employee name is Invalid ")
     public String name;
+
+    @Min(value = 500, message = "Min Wage should be more than 500")
     public long salary;
+
+    public String gender;
+
+    public LocalDate startDate;
+
+    public String note;
+
+    public String profilePic;
+
+    public String department;
 }
--- src/main/java/com/bridgelabz/employeepayroll/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayroll/model/EmployeePayrollData.java
@@ -1,7 +1,11 @@
 package com.bridgelabz.employeepayroll.model;
 
 import com.bridgelabz.employeepayroll.dto.EmployeePayrollDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
 
 
 @Data
@@ -9,11 +13,23 @@ public class EmployeePayrollData {
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private String departments;
+
 
     public EmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO){
         this.employeeId = empId;
         this.name = employeePayrollDTO.name;
         this.salary = employeePayrollDTO.salary;
+        this.gender = employeePayrollDTO.gender;
+        this.note = employeePayrollDTO.note;
+        this.startDate = employeePayrollDTO.startDate;
+        this.profilePic = employeePayrollDTO.profilePic;
+        this.departments = employeePayrollDTO.department;
     }
 }
 
